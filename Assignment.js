f167e9267895d5697491a21dbf541d1e9eb0c0c7
hachi999 <samueldaniel2874@gmail.com>
2024-11-13 18:36:45 +0100

    Add files via upload

---
+++ Assignment.js
@@ -0,0 +1,19 @@
+var num= 22/7;
+var num2= 6;
+var num3= 8  ;
+var result=num * num2 * num3;
+console.log(result)
+
+var num4= 1/2;
+var num5= 2;
+var num6= 6
+var result2= num4 * num5 * num6;
+console.log(result2)
+
+var num7 =20;
+var num8 =4;
+var num9 =5;
+var num10 =10;
+var result3= num7 * num8 * num9 / num10;
+console.log(result3)
+
