eae3de160413f9d393911dcf8e50d19032f31a5c
hachi999 <samueldaniel2874@gmail.com>
2024-11-13 21:51:07 +0100

    Update Assignment.js

--- Assignment.js
+++ Assignment.js
@@ -1,15 +1,18 @@
+(number1)Area=pi * radius2
 var num= 22/7;
 var num2= 6;
 var num3= 8  ;
 var result=num * num2 * num3;
 console.log(result)
 
+(number2)Area=1/2 * base * height
 var num4= 1/2;
 var num5= 2;
 var num6= 6
 var result2= num4 * num5 * num6;
 console.log(result2)
 
+(number3)Simple interest=Principal * rate * time/100
 var num7 =20;
 var num8 =4;
 var num9 =5;
@@ -17,3 +20,9 @@ var num10 =10;
 var result3= num7 * num8 * num9 / num10;
 console.log(result3)
 
+Results
+PS C:\Users\EKWUOMA GIFT\Desktop\Java> node Assignment.js
+150.85714285714286
+6
+40
+PS C:\Users\EKWUOMA GIFT\Desktop\Java>
